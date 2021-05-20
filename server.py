245e88e22465c663f4434f4bb677f2062de3f79d
nicolasvilleneuve <80601403+nicolasvilleneuve@users.noreply.github.com>
2021-05-20 18:47:54 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import os
 import csv
-import smtplib
-
-igor_email = "THISEMAILACCOUNTISNTREAL@gmail.com"
-igor_password = "THISPASSWORDISNTREAL"
-my_email = 'THISEMAILISNTREAL@gmail.com'
 
 app = Flask(__name__)
 print(__name__)
@@ -35,14 +30,6 @@ def write_to_csv(data):
         csv_writer = csv.writer(real_database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([full_name,email,subject,message])
 
-        with smtplib.SMTP('smtp.gmail.com') as connection:
-            connection.starttls()
-            connection.login(igor_email, igor_password)
-            connection.sendmail(from_addr=igor_email, to_addrs=my_email, msg=f"Subject: Someone Contacted You Through Your Website "
-                                                                       f"\n\n Their name is {full_name}, "
-                                                                                    f"email is: {email},"
-                                                                                    f"subject is:{subject}"
-                                                                                    f"and message is: {message}")
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
